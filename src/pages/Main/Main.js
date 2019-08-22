31587a6af918b082de2bbae81717eac87485a02c
Clement Balestrat <>
2019-08-22 10:35:31 +1000

    fix path

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Dashboard from '../../screens/MintrTabs/Dashboard';
+import Dashboard from '../../screens/Dashboard';
 import MintrPanel from '../../screens/MintrPanel';
 
 const Main = () => {
