51e0470f578b5c6fb19810cf6703c62758559cbd
Max Alekseenko <nnwen0609@gmail.com>
2019-09-20 00:03:42 +0300

    update file input styles

--- src/screens/SETHPoolRewardDistribution/CsvLoader.js
+++ src/screens/SETHPoolRewardDistribution/CsvLoader.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import papaparse from 'papaparse';
+import styled from 'styled-components';
+
+import { ButtonSecondaryLabel } from '../../components/Typography';
 
 export default ({ onDataLoaded }) => {
   const onFileChange = event => {
@@ -15,5 +18,37 @@ export default ({ onDataLoaded }) => {
       fileReader.readAsText(event.target.files[0]);
     }
   }
-  return <input type="file" name="file" accept=".csv" onChange={onFileChange}/>;
+  return (
+    <div>
+      <Input type="file" name="file" accept=".csv" onChange={onFileChange} id="file" />
+      <Label for="file"><ButtonSecondaryLabel>browse files</ButtonSecondaryLabel></Label>
+    </div>
+  );
 }
+
+const Input = styled.input`
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+`;
+
+const Label = styled.label`
+  width: ${props => (props.width ? props.width + 'px' : '400px')};
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 72px;
+  border-radius: 5px;
+  text-transform: uppercase;
+  border: 2px solid ${props => props.theme.colorStyles.buttonPrimaryBg};
+  cursor: pointer;
+  background-color: transparent;
+  transition: all ease-in 0.1s;
+  &:hover {
+    background-color: ${props => props.theme.colorStyles.buttonTertiaryBgFocus};
+  }
+`;
