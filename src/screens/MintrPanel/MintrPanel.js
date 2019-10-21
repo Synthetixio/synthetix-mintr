5397eb085aa46f62b0b55e9ff3fe1f7df36a38eb
Clement Balestrat <>
2019-10-21 12:23:27 +1100

    transaction and depot tabs locked

--- src/screens/MintrPanel/MintrPanel.js
+++ src/screens/MintrPanel/MintrPanel.js
@@ -22,7 +22,7 @@ const TabRow = ({ state }) => {
       <TabButton
         key={tab}
         isSelected={tab === currentTab}
-        disabled={[].includes(tab)}
+        disabled={['transactionsHistory', 'depot'].includes(tab)}
         onClick={() => updateCurrentTab(tab, dispatch)}
       >
         {/* i18next-extract-disable-next-line */}
