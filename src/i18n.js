a72b47dfbbed0f7e8854df180fc5c177eef1f83e
Clement Balestrat <>
2019-10-25 17:31:38 +1100

    i18n update

--- src/i18n.js
+++ src/i18n.js
@@ -1,18 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translations from 'synthetix-translations';
+import snxTranslations from 'synthetix-translations';
 
 i18n
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
-		resources: translations['synthetix-mintr'],
+		resources: snxTranslations['synthetix-mintr'],
 		fallbackLng: 'en',
 		detection: {
 			order: ['navigator'],
 		},
-		debug: true,
 		react: {
 			useSuspense: true,
 			wait: true,
