95400369d7f28747997f369571b701821227668e
Clement Balestrat <>
2019-10-25 13:35:24 +1100

    i18n config updated to read custom JSON from synthetix-translations lib

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,19 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-xhr-backend';
+
+import translations from 'synthetix-translations';
 
 i18n
-	.use(Backend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
+		resources: translations['synthetix-mintr'],
+		fallbackLng: 'en',
 		detection: {
 			order: ['navigator'],
 		},
-		// debug: true,
-		fallbackLng: 'en',
-		interpolation: {
-			escapeValue: false,
-		},
+		debug: true,
 		react: {
 			useSuspense: true,
 			wait: true,
