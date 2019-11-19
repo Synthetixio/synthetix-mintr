ddd13ae210b76c315173f5365dce508aeed42c87
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2019-11-19 18:56:28 +1100

    Add derivation path to wallet duck state

--- src/ducks/wallet.js
+++ src/ducks/wallet.js
@@ -1,5 +1,6 @@
 const UPDATE_WALLET_STATUS = 'WALLET/UPDATE_WALLET_STATUS';
 const UPDATE_WALLET_PAGINATOR_INDEX = 'WALLET/UPDATE_WALLET_PAGINATOR_INDEX';
+const SET_DERIVATION_PATH = 'WALLET/SET_DERIVATION_PATH';
 
 // Reducer
 export default (state, action) => {
@@ -10,12 +11,21 @@ export default (state, action) => {
 		case UPDATE_WALLET_PAGINATOR_INDEX: {
 			return { ...state, walletPaginatorIndex: action.payload };
 		}
+		case SET_DERIVATION_PATH: {
+			return { ...state, derivationPath: action.payload };
+		}
 		default:
 			return state;
 	}
 };
 
 // Actions
+export const setDerivationPath = (path, dispatch) => {
+	return dispatch({
+		type: SET_DERIVATION_PATH,
+		payload: path,
+	});
+};
 export const updateWalletStatus = (walletStatus, dispatch) => {
 	return dispatch({
 		type: UPDATE_WALLET_STATUS,
