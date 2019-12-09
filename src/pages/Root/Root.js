43d82a79929ac35494a2f267c543b0d1ebb2cd60
Clement Balestrat <>
2019-12-09 11:11:18 +1100

    netlify env variable test

--- src/pages/Root/Root.js
+++ src/pages/Root/Root.js
@@ -45,7 +45,7 @@ const Root = () => {
 		}
 	}, []);
 	useEffect(() => {
-		console.log(process.env.CONTEXT);
+		console.log(process.env.REACT_APP_CONTEXT);
 		// if (process.env.CONTEXT !== 'production') return;
 		const init = async () => {
 			const { networkId } = await getEthereumNetwork();
