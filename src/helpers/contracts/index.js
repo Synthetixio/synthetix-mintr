eb2296fbc07e6dae23257dd54f3e4e040b5bd971
Clement Balestrat <clement@synthetix.io>
2020-03-11 17:19:46 +1100

    curvepool added

--- src/helpers/contracts/index.js
+++ src/helpers/contracts/index.js
@@ -1,5 +1,7 @@
 import unipool from './unipool';
 import uniswap from './uniswap';
+import curvepool from './curvepool';
+import curveLPToken from './curveLPToken';
 import synthSummary from './synthSummary';
 
-export { unipool, uniswap, synthSummary };
+export { unipool, uniswap, curvepool, curveLPToken, synthSummary };
--- src/screens/MintrTabs/index.js
+++ src/screens/MintrTabs/index.js
@@ -2,6 +2,6 @@ import Home from './Home';
 import Depot from './Depot';
 import Transactions from './Transactions';
 import Escrow from './Escrow';
-import UniPool from './UniPool';
+import LPRewards from './LPRewards';
 
-export { Home, Depot, Transactions, Escrow, UniPool };
+export { Home, Depot, Transactions, Escrow, LPRewards };
