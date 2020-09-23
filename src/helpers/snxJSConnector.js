4ab71c20c21b8138f9a5233b654a1c9c76cc1327
Clement Balestrat <clement.balestrat@gmail.com>
2020-09-23 14:45:09 +1000

    signer fix

--- src/helpers/snxJSConnector.js
+++ src/helpers/snxJSConnector.js
@@ -183,13 +183,10 @@ export const setSigner = async ({ type, networkId, derivationPath, networkName }
 		getSignerConfig({ type, networkId, derivationPath, networkName })
 	);
 
-	const provider = signer._optimism;
-	provider._ethereum = null;
-	console.log('haha', signer._optimism);
 	snxJSConnector.setContractSettings({
 		networkId,
 		signer,
-		provider,
+		provider: signer.provider,
 	});
 };
 
