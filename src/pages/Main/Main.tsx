060eb3ff10288627987b292f0fe1facf532151c4
AndyTCF <andy.c.f.98@gmail.com>
2020-10-08 20:13:44 +1100

    remove unused variable

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import Dashboard from 'screens/Dashboard';
 import MintrPanel from 'screens/MintrPanel';
 import L2Banner from 'components/Banner/L2Banner';
-import l2Wallets from 'assets/data/l2-wallets.json';
+// import l2Wallets from 'assets/data/l2-wallets.json';
 const Main: FC<MainProps> = ({ wallet }) => {
-	const bannerIsVisible = l2Wallets.find(
-		({ address }) => address.toLowerCase() === wallet.toLowerCase()
-	);
+	// const bannerIsVisible = l2Wallets.find(
+	// 	({ address }) => address.toLowerCase() === wallet.toLowerCase()
+	// );
 	return (
 		<MainWrapper>
 			<L2Banner />
