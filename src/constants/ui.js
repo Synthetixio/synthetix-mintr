3cd8c26c22e3479af80862abbe553db08a4ea403
Clement Balestrat <clement.balestrat@gmail.com>
2020-10-09 21:35:03 +1100

    reducing timer interval

--- src/constants/ui.js
+++ src/constants/ui.js
@@ -3,4 +3,4 @@ import keyBy from 'lodash/keyBy';
 const PAGES = ['LANDING', 'WALLET_SELECTION', 'MAIN', 'L2ONBOARDING'];
 export const PAGES_BY_KEY = keyBy(PAGES);
 
-export const INTERVAL_TIMER = 5 * 60 * 1000;
+export const INTERVAL_TIMER = 2 * 60 * 1000;
