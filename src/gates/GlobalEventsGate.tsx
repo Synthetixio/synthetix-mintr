a7de4466c31387d3cbe74d3c439d0b979670fb23
AndyTCF <andy.c.f.98@gmail.com>
2020-10-12 14:17:42 +1100

    Finalise mintr refetch

--- src/gates/GlobalEventsGate.tsx
+++ src/gates/GlobalEventsGate.tsx
@@ -5,16 +5,6 @@ import { RootState } from 'ducks/types';
 import { getCurrentWallet } from 'ducks/wallet';
 import { setSystemUpgrading } from 'ducks/app';
 
-import snxJSConnector from 'helpers/snxJSConnector';
-import {
-	EXCHANGE_EVENTS,
-	TRANSFER_EVENTS,
-	SYSTEM_STATUS_EVENTS,
-	EXCHANGE_RATES_EVENTS,
-	REWARD_ESCROW_EVENTS,
-	SYNTHETIX_ESCROW_EVENTS,
-} from 'constants/events';
-
 const mapStateToProps = (state: RootState) => ({
 	currentWallet: getCurrentWallet(state),
 });
@@ -27,35 +17,6 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
 const GlobalEventsGate: FC<PropsFromRedux> = ({ currentWallet, setSystemUpgrading }) => {
-	useEffect(() => {
-		if (!currentWallet) return;
-		// const {
-		// 	//@ts-ignore
-		// 	snxJS: { sUSD, FeePool, Synthetix, RewardEscrow, SynthetixEscrow },
-		// } = snxJSConnector;
-
-		// SynthetixEscrow.contract.on(SYNTHETIX_ESCROW_EVENTS.VESTED, (address: string) => {
-		// 	if (address === currentWallet) {
-		// 		fetchEscrowRequest();
-		// 	}
-		// });
-		// RewardEscrow.contract.on(REWARD_ESCROW_EVENTS.VESTED, (address: string) => {
-		// 	if (address === currentWallet) {
-		// 		fetchEscrowRequest();
-		// 	}
-		// });
-
-		// return () => {
-		// Object.values(SYNTHETIX_ESCROW_EVENTS).forEach(event =>
-		// 	SynthetixEscrow.contract.removeAllListeners(event)
-		// );
-		// Object.values(REWARD_ESCROW_EVENTS).forEach(event =>
-		// 	RewardEscrow.contract.removeAllListeners(event)
-		// );
-		// };
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [currentWallet]);
-
 	useEffect(() => {
 		// const {
 		// 	//@ts-ignore
